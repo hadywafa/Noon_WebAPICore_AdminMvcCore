3bcf06ece9fb62249a04957bddc86845d56bc19f
Wardady11 <emad.naeim11@gmail.com>
2022-04-26 16:01:44 +0200

    - Fixed Changes

--- NoonAdminMvcCore/Controllers/UserController.cs
+++ NoonAdminMvcCore/Controllers/UserController.cs
@@ -159,7 +159,10 @@ namespace NoonAdminMvcCore.Controllers
                     // Initialize new Address
                     var address = new Address
                     {
-                        User = user, Street = model.Street, City = model.City, PostalCode = model.PostalCode
+                        User = user, 
+                        Street = model.Street, 
+                        City = model.City, 
+                        PostalCode = model.PostalCode
                     };
 
                     // Add to the Address table
@@ -187,6 +190,16 @@ namespace NoonAdminMvcCore.Controllers
                     user.PhoneNumber = model.PhoneNumber;
                     user.Addresses.FirstOrDefault()!.City = model.City;
                     user.Addresses.FirstOrDefault()!.Street = model.Street;
+
+                    // Get current Role
+                    var role = await _userManager.GetRolesAsync(user);
+
+                    // Remove Current Role
+                    await _userManager.RemoveFromRoleAsync(user, role.FirstOrDefault());
+
+                    //Add new Role
+                    await _userManager.AddToRoleAsync(user, model.Role);
+
                     _unitOfWork.Save();
                 }
             }
@@ -198,12 +211,13 @@ namespace NoonAdminMvcCore.Controllers
         public async Task<ActionResult> Edit(string id)
         {
             var user = _userRepository.Find(u => u.Id == id, u => u.Addresses);
+
             if (user == null)
             {
                 return NotFound();
             }
 
-            var role = _userManager.GetRolesAsync(user).Result.AsQueryable().FirstOrDefault();
+            var role = await _userManager.GetRolesAsync(user);
 
             var userViewModel = new UserViewModel
             {
@@ -213,7 +227,7 @@ namespace NoonAdminMvcCore.Controllers
                 Email = user.Email,
                 Password = "",
                 Balance = user.Balance,
-                Role = role,
+                Role = role.FirstOrDefault(),
                 IsActive = user.IsActive,
                 PhoneNumber = user.PhoneNumber,
                 Street = user.Addresses.FirstOrDefault()?.Street,
@@ -223,7 +237,7 @@ namespace NoonAdminMvcCore.Controllers
             return View("UserForm", userViewModel);
         }
 
-        public ActionResult Suspend(string id)
+        public async Task<ActionResult> SuspendAsync(string id)
         {
             // get the user
             var user = _userRepository.GetById(id);
@@ -237,12 +251,26 @@ namespace NoonAdminMvcCore.Controllers
             // save updates
             _unitOfWork.Save();
 
-            // get all users
-            var users = _userManager.Users;
-            return PartialView("_UserPartial", users.ToList());
+            // Get current Role
+            var role = await _userManager.GetRolesAsync(user);
+
+            // Initializing a List of Users
+            var users = new List<User>();
+
+            // Get Users by role
+            var data = await _userManager.GetUsersInRoleAsync(role.FirstOrDefault());
+
+            // Loop in users including their addresses
+            foreach (var _u in data)
+            {
+                var _user = _userRepository.Find(u => u.Id == _u.Id, u => u.Addresses);
+                users.Add(_user);
+            }
+
+            return PartialView("_UserPartial", users);
         }
 
-        public ActionResult Activate(string id)
+        public async Task<ActionResult> ActivateAsync(string id)
         {
             // get the user
             var user = _userRepository.GetById(id);
@@ -256,9 +284,23 @@ namespace NoonAdminMvcCore.Controllers
             // save updates
             _unitOfWork.Save();
 
-            // get all users
-            var users = _userManager.Users;
-            return PartialView("_UserPartial", users.ToList());
+            // Get current Role
+            var role = await _userManager.GetRolesAsync(user);
+
+            // Initializing a List of Users
+            var users = new List<User>();
+
+            // Get Users by role
+            var data = await _userManager.GetUsersInRoleAsync(role.FirstOrDefault());
+
+            // Loop in users including their addresses
+            foreach (var _u in data)
+            {
+                var _user = _userRepository.Find(u => u.Id == _u.Id, u => u.Addresses);
+                users.Add(_user);
+            }
+
+            return PartialView("_UserPartial", users);
         }
     }
 }
