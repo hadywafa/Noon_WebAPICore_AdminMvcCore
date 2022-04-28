b2e143ead80894794fcd82dc7ee34d410c694821
Hady Wafa <97hadywafa@gmail.com>
2022-04-29 01:27:51 +0200

    Hw Nested Category as JSON

--- JWTAuth/Controllers/ProductsController.cs
+++ JWTAuth/Controllers/ProductsController.cs
@@ -1,9 +1,14 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using BL.AppPolicy;
 using Microsoft.AspNetCore.Mvc;
 using EFModel.Models.EFModels;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.Data.SqlClient;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using Repository.GenericRepository;
 using Repository.UnitWork;
 
@@ -42,6 +47,35 @@ namespace JWTAuth.Controllers
             return book;
         }
 
+        [HttpGet("GetAllCategories")]
+        public string GetAllCategories()
+        {
+            var queryWithForJson = "exec [dbo].[spGetJson]";
+            using (var conn = new SqlConnection("Data Source=.;Initial Catalog=Noon_API_MVC;Integrated Security=True"))
+            {
+                using (var cmd = new SqlCommand(queryWithForJson, conn))
+                {
+                    conn.Open();
+                    var jsonResult = new StringBuilder();
+                    var reader = cmd.ExecuteReader();
+                    if (!reader.HasRows)
+                    {
+                        jsonResult.Append("[]");
+                    }
+                    else
+                    {
+                        while (reader.Read())
+                        {
+                            jsonResult.Append(reader.GetValue(0).ToString());
+                        }
+                    }
+                    var json  = JsonConvert.DeserializeObject(jsonResult.ToString());
 
+                    return json.ToString();
+
+                }
+
+            }
+        }
     }
 }
