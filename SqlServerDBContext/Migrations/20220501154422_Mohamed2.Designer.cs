b058fada3e02b386adc69c0b4c660eaab9b9c641
Mohamed SayedAhmed Hassani <msayeda70@gmail.com>
2022-05-01 18:45:30 +0200

    Mohamed OrderController OrderModelChanges Adding OrderViewModel

--- SqlServerDBContext/Migrations/20220501154422_Mohamed2.Designer.cs
+++ SqlServerDBContext/Migrations/20220501154422_Mohamed2.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SqlServerDBContext;
 
 namespace SqlServerDBContext.Migrations
 {
     [DbContext(typeof(SqlContext))]
-    partial class SqlContextModelSnapshot : ModelSnapshot
+    [Migration("20220501154422_Mohamed2")]
+    partial class Mohamed2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
