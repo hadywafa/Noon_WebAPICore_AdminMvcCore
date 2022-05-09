772ba36fe101611d54607ed27ad620133ca39162
Hady Wafa <97hadywafa@gmail.com>
2022-05-09 18:40:49 +0200

    Hw => Get All Carts

---
+++ BL/ViewModels/ResponseVModels/VmCartProduct.cs
@@ -0,0 +1,10 @@
+﻿using BL.ViewModels.RequestVModels;
+
+namespace BL.ViewModels.ResponseVModels
+{
+    public class VmCartProduct
+    {
+        public int Quantity { get; set; }
+        public VmProduct Product { get; set; }
+    }
+}
