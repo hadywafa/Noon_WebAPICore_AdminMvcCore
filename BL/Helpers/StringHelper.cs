b5376b893a42f9f8400bd022c7af5cad15d6f8ec
Hady Wafa <97hadywafa@gmail.com>
2022-05-12 14:26:30 +0200

    Hw => Image Url

--- BL/Helpers/StringHelper.cs
+++ BL/Helpers/StringHelper.cs
@@ -8,7 +8,8 @@ namespace BL.Helpers
     {
         public static string ToImageUrl(this string image)
         {
-            return $"http://localhost:25248/Images/{image}";
+            //return $"http://localhost:25248/Images/{image}";
+            return $"http://7699-53329.el-alt.com/Images/{image}";
         }
     }
 }
